f4b87700cf65463497af012d51cd44a91021c93b
soumilagarwal <39463423+soumilagarwal@users.noreply.github.com>
2020-11-03 15:20:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,77 +1,50 @@
-"""
-This program aims to detect plagiarism using Locality Sensitive Hahsing(LSH)
-
-Process:
-    - shingling: converting documents into set of tokens. each token is called 
-        as shingle which is a sequence of characters present in file. This process
-        generates a sparse incidence matrix
-    - minhashing: as incidence matrix is sparse, this step aims to reduce the 
-        size of matrix by producing signature for each document present in the 
-        incidence matrix
-    - lsh: dividing signature matrix into horizontal bands and applying hash
-        functions on them to determing which documents fall in same bucket
-
-NOTE: if pickle(binary) files exist for each operation, they will be used 
-    instead of generating them again(to save time). rename or delete the file
-    to start afresh.
-"""
-
-import time, os
-import shingling
+import time
+import shingler
 import minhashing
 import lsh
-import statistics
+import numpy as np
+import pandas as pd
 
-def startLSH():
-    print("\n*** Plagiarism detection using LSH ***\n")
 
-    # step 1: shingling
-    timer_start = time.time()   # start timer
-    folderpath = "corpus"       # path to corpus
-    extension=".txt"            # specified extensions to read. Set to None to ignore extension
-    shingle_size = 4            # size of shingle: 8-12 is reommended
-    shingle_matrix, files = shingling.get_shingle_matrix(folderpath, shingle_size, extension)
-    print(shingle_matrix.shape)
-    print(f"Time taken for shingling: {time.time()-timer_start}")
+def main():
+    data = pd.read_csv('human_data.csv')
+    sequences=data['sequence']
+    # data = pd.read_csv("test_data.txt")
+    # sequences = data['document']
+    print("Number of documents = ", len(sequences))
 
-    # step 2: min-hashing
-    start_time = time.time()    # start timer
-    no_of_hash_functions = 50   # specify no of hash functions for signature matrix
-    signature_matrix = minhashing.generate_signature_matrix(shingle_matrix, no_of_hash_functions)
-    print(f"Time taken for minhashing: {time.time()-start_time}")
+    start = time.time()
+    shingles=[]
+    shingle_matrix = []
+    shingle_size = 4
+    shingles = shingler.generate_shingles(sequences, shingle_size)
+    shingle_matrix = shingler.initialize_matrix(sequences, shingles)    #shingle matrix 336x4380
+    end = time.time()
 
-    # step 3: LSH(Locality sensitive hashing)
-    start_time = time.time()    # start timer
-    r = 2
-    buckets_list = lsh.get_bucket_list(signature_matrix, r)
-    print(f"Time taken for lsh: {time.time()-start_time}")
+    print("Number of shingles found = ", len(shingles))
+    # print(shingles)
+    print("Shingle matrix size is ")
+    print(len(shingles), " x ", len(shingle_matrix[0]))
+    print("Time taken for shingling = ",round((end - start),2),"s\n")
 
-    # preprocessing done. ask file from user to check plagiarism
-    sim_type = "jaccard"
-    while True:
-        test_file = input("Enter path of file: ")
-        if test_file == "EXIT":
-            break;
-        if not os.path.exists(test_file):
-            print(">> The given path does not exist.")
-            continue
-        for name, num in files:
-            if test_file == name:
-                test_file = int(num)
-        # test_file = int(input("Enter path of file: "))
-        threshold = float(input("Enter threshold: "))
+    start = time.time()
+    sig_matrix = minhashing.minhasher(shingles, shingle_matrix)
+    end = time.time()
 
-        print(f"Given file: {statistics.get_file_name(test_file, files)}")
-        similar_docs = lsh.find_similar_docs(test_file, buckets_list, signature_matrix, r)
-        output = statistics.compute_similarity(test_file, similar_docs, shingle_matrix, sim_type)
+    print("Signature matrix size is ")
+    print(len(sig_matrix), " x ", len(sig_matrix[0]))
+    # print("signature matrix is\n",sig_matrix)
+    print("Time taken for minhashing = ",round((end - start),2),"s\n")
 
-        for file_id, score in output:
-            print(f"{statistics.get_file_name(file_id, files)}\t{score}")
-        
-        print(f"Precision: {statistics.precision(threshold, output)}")
-        print(f"Recall: {statistics.recall(threshold, test_file, len(files), output, shingle_matrix, sim_type)}")
-
-    print("\n*** End of Program ***\n")
-
+    start = time.time()
+    buckets_list = lsh.bucket_list(sig_matrix)
+    end = time.time()
+    print("Buckets generated successfully.")
+    # print("Bucket lists are \n",buckets_list)
+    print("Time taken for bucket generation = ",round((end - start),2),"s\n")
+    
+    #query_dna = input("Enter DNA sequence : ") 
+    #sim_docs = lsh.find_sim_docs(query_dna, buckets_list, sig_matrix)
+    # final = similarity.compute_similarity(query_dna, sim_docs, shingle_matrix)
 if __name__ == "__main__":
-    startLSH()
+    main()
